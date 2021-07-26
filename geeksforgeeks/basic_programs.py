cfc5954481c2729ce09e12c0713e214fee65640d
saimanivarma <varma338@gmail.com>
2021-07-26 11:18:03 +0530

    Update basic_programs.py
    added factorial code

--- geeksforgeeks/basic_programs.py
+++ geeksforgeeks/basic_programs.py
@@ -2,6 +2,13 @@
 # number2 = 21
 # print(number1+number2)
 #
-number1 = 34
-number2 = 44
-print(max(number1, number2))
+# number1 = 34
+# number2 = 44
+# print(max(number1, number2))
+
+import math
+def factorial(number):
+    return math.factorial(number)
+
+result = factorial(7)
+print(result)
